cc14e42ecbe34fcdf71fc56579fc9a8c89a84ca6
SonYoungsung <sysgigigi@gmail.com>
2025-11-21 14:07:58 +0900

    Update auth.ts

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -16,7 +16,9 @@ const HARDCODED_ADMINS = [
   // TODO: 실제 관리자 지갑 주소로 변경하세요!
   // 예시:
   "0xF9Fa94D45C49e879E46Ea783fc133F41709f3bc7",
-  // 여기에 추가 관리자 주소를 넣으세요
+  "0x7f88539538ae808e45e23ff6c2b897d062616c4e",
+  "0x9f1474b5b01940af4f6641bdcbcf8af3ca5197ec",
+  "0x3d827286780dBc00ACE4ee416aD8a4C5dAAC972C",
 ].map((addr) => addr.toLowerCase());
 
 export const ALLOWED_ADMINS =
